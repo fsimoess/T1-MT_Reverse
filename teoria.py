f7d9d4e74e9b41cfaac21e02403f8af430218dd2
Filipe <fsmendonca@inf.ufsm.br>
2018-10-17 02:11:57 +0000

    Funcionando reversão para o entrada.txt

--- teoria.py
+++ teoria.py
@@ -34,14 +34,14 @@ class Fita(object):
 	def read_fita(self):
 		return self.fita[self.head]
 
-	def write_fite(self, aux):
+	def write_fita(self, aux):
 		self.fita = self.fita[:self.head] + aux + self.fita[self.head + 1:]
 
 	def move_left(self):
 		if self.head == 0:
 			return
-		if self.fita[self.head] == 'B' and self.fita[self.head + 1] == 'B' and self.head + 2 == len(self.fita):
-			self.fita = self.fita[:self.head+1]
+		if self.head == len(self.fita)-1 or self.fita[self.head] == "B":
+			self.fita = self.fita[:self.head-1] + "B"
 		self.head = self.head - 1
 
 	def move_right(self):
@@ -49,37 +49,70 @@ class Fita(object):
 		if self.head == len(self.fita):
 			self.fita = self.fita + "B"
 
+	def write_history(self, aux, dir):
+		if dir == "x" or dir == "D":
+			self.fita = self.fita[:self.head] + aux + self.fita[self.head + 1:]
+		if dir == "D" or dir == "x":
+			self.move_right()
+		if dir == "E" or dir == "0":
+			self.move_left()
+
 class Maquina_Turing(object):
 	def __init__(self, entrada):
 		self.fita1 = Fita(entrada)
 		self.fita2 = Fita("")
 		self.fita3 = Fita("")
 
-	def print_fita(self):
-		print(self.fita1)
+def MT_revert(newState, _f1, _f2, _f3, oldState, f1, f2, f3):
 
-def control(str1, fita, mode, str2, maquina):
+	if f1 == "/" and _f1 == "D":
+		_f1 = f1
+		f1 = "E"
+	if f1 == "/" and _f1 == "E":
+		_f1 = f1
+		f1 = "D"
+	if f2 == "/" and _f2 == "D":
+		_f2 = f2
+		f2 = "E"
+	if f2 == "/" and _f2 == "E":
+		_f2 = f2
+		f2 = "D"
+	if f3 == "/" and _f3 == "D":
+		_f3 = f3
+		f3 = "E"
+	if f3 == "/" and _f3 == "E":
+		_f1 = f1
+		f1 = "D"
+
+	transitions = "(" + newState + "," + _f1 + "," + _f2 + "," + _f3 + ")=(" + oldState + "," + f1 + "," + f2 + "," + f3 + ")"
+	return transitions
+
+def control(str1, oldState, fita, mode, str2, maquina):
 	if mode == "Move":
-		print('Movimento na fita', fita)
+		#print('Movimento na fita', fita)
 		if str2 == "D" and fita==1:
 			maquina.fita1.move_right()
+			maquina.fita2.write_history(oldState, str2)
 		if str2 == "E" and fita==1:
 			maquina.fita1.move_left()
+			maquina.fita2.write_history(oldState, str2)
 		if str2 == "D" and fita==2:
-			maquina.fita2.move_right()
+			maquina.fita2.write_history(oldState, str2)
 		if str2 == "E" and fita==2:
-			maquina.fita2.move_left()
+			maquina.fita2.write_history(oldState, str2)
+			print('oi',maquina.fita2.head)
 		if str2 == "D" and fita==3:
 			maquina.fita3.move_right()
+			maquina.fita2.write_history(oldState, str2)
 		if str2 == "E" and fita==3:
 			maquina.fita3.move_left()
+			maquina.fita2.write_history(oldState, str2)
 	if mode == "RW":
-		print('Leitura e Escrita na fita', fita)
-		if str1 != "B":
-			maquina.fita2.write_fite(maquina.fita1.read_fita())
-			maquina.fita1.write_fite("x")
+		#print('Leitura e Escrita na fita', fita)
+		maquina.fita1.write_fita(str2)
+		maquina.fita2.write_history(oldState, str2)
 
-def transitions_MT(var, maquina):
+def transitions_MT(var, maquina, transitions_revert, palavra):
 	oldState = var[1]
 	f1 = var[3]
 	f2 = var[5]
@@ -90,17 +123,11 @@ def transitions_MT(var, maquina):
 	_f3 = var[17]
 
 	if f1 == "/":
-		control(f1, 1, "Move", _f1, maquina)
+		control(f1, oldState, 1, "Move", _f1, maquina)
 	if f1 != "/":
-		control(f1, 1, "RW", _f1, maquina)
-	if f2 == "/":
-		control(f2, 2, "Move", _f2, maquina)
-	if f2 != "/":
-		control(f2, 2, "RW", _f2, maquina)
-	if f3 == "/":
-		control(f3, 3, "Move", _f3, maquina)
-	if f3 != "/":
-		control(f3, 3, "RW", _f3, maquina)
+		control(f1, oldState, 1, "RW", _f1, maquina)
+
+	transitions_revert.append(MT_revert(newState,_f1,_f2,_f3,oldState,f1,f2,f3))
 
 def main(args):
 
@@ -110,7 +137,9 @@ def main(args):
 	states = list()
 	alfabeto_fita = list()
 	transitions = list()
+	transitions_revert = list()
 	enter_fita = list()
+	exit_fita = list()
 
 	for x in open(args[1], "r"):
 		entrada.append(x[:-1])
@@ -135,18 +164,43 @@ def main(args):
 	print('______________________________________')
 	print('Dados das Fitas: ')
 	maquina = Maquina_Turing(enter_fita)
-	print(maquina.fita1.fita)
-	print(maquina.fita2.fita)
-	print(maquina.fita3.fita)
-
+	print('Estado inicial da Fita 1: ',maquina.fita1.fita)
+	print('Estado inicial da Fita 2: ',maquina.fita2.fita)
+	print('Estado inicial da Fita 3: ',maquina.fita3.fita)
 	print('______________________________________')
-	print('Transições: ')
+
 	for x in transitions:
 		print(x)
-		transitions_MT(x, maquina)
-		print('Estado atual da Fita 1: ',maquina.fita1.fita)
-		print('Estado atual da Fita 2: ',maquina.fita2.fita)
-		print('Estado atual da Fita 1: ',maquina.fita3.fita)
-		print('______________________________________')
+		transitions_MT(x, maquina, transitions_revert, enter_fita)
+
+	transitions_revert.reverse()
+	transitions = list()
+
+	print('Primeiro passo da MT Reversível:')
+	print('Estado atual da Fita 1: ',maquina.fita1.fita)
+	print('Estado atual da Fita 2: ',maquina.fita2.fita)
+	print('Estado atual da Fita 3: ',maquina.fita3.fita)
+	print('______________________________________')
+	
+	exit_fita = maquina.fita1.fita
+	maquina.fita3.fita = exit_fita
+
+	print('Cópia da saída, segundo passo da MT Reversível:')
+	print('Estado atual da Fita 1: ',maquina.fita1.fita)
+	print('Estado atual da Fita 2: ',maquina.fita2.fita)
+	print('Estado atual da Fita 3: ',maquina.fita3.fita)
+	print('______________________________________')
+
+	for x in transitions_revert:
+		print(x)
+		transitions_MT(x, maquina, transitions, exit_fita)
+
+	print('Reversão da saída, terceiro passo da MT Reversível:')
+	print('Estado atual da Fita 1: ',maquina.fita1.fita)
+	print('Estado atual da Fita 2: ',maquina.fita2.fita)
+	print('Estado atual da Fita 3: ',maquina.fita3.fita)
+	print('______________________________________')
+
+	#Salvar a nova entrada na fita 3
 
 if __name__ == "__main__": sys.exit(main(sys.argv))
